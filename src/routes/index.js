36f02aecc04c01f388756916bcb88eb8b035176f
ArjDev69 <bruno96araujo@gmail.com>
2022-10-09 10:26:45 -0300

    feat: develop page register user

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import Login from '../pages/Login'
+import RegisterUser from '../pages/User/Register'
 import Route from './Route';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
+      <Route path="/register/user" exact component={RegisterUser} />
       {/* <Route path="/register" exact component={SignUp} /> */}
 
       {/* <Route path="/dashboard" isPrivate component={Dashboard} /> */}
